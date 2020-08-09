3c58f947c736f09bf20113b6742c87e4b53f93a4
김창연 <changyeonkim@gimchang-yeon-ui-MacBookAir.local>
2020-08-09 10:04:17 +0900

    props test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const foodILike = [
+  {
+    id : 1,
+    name : "kimchi"
+  },
+  {
+    id : 2,
+    name : "banana"
+  },
+  {
+    id : 3,
+    name : "hahaha"
+  },
+  {
+    id : 4,
+    name : "tomato"
+  },
+  {
+    id : 5,
+    name : "potato"
+  }
+]
+
+function Food({name}){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+    </div>
+  )
+}
+
+function renderFood(dish){
+    return  <Food key = {dish.id} name ={dish.name}></Food>
+  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
+/*
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key = {dish.id} name ={dish.name}></Food>
+      ))
 
+      }
+    </div>
+  );
+}
+*/
 export default App;
